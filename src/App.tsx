4e6b26398f14fb78369374d683873264bd6bf63e
Will Baxter <bax.will@googlemail.com>
2024-11-08 10:50:53 +0000

    GSAP installed, text rotation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import './App.css'
+import { gsap } from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { useState } from 'react';
+
+gsap.registerPlugin(useGSAP);
+
 
 function App() {
+  const [animate, setAnimate] = useState<boolean>(false)
+
+  useGSAP(() => {
+    gsap.to(".text", { rotation: 360, repeat: -1, duration: 5, ease: "linear" })
+  }, [animate])
+
   return (
     <>
-    <p>Let's go</p>
+    <div className="flex flex-col h-screen items-center justify-center gap-6">
+    <p className="text">Let's go</p>
+    <button onClick={() => setAnimate(!animate)} className="border-2 p-2">{animate ? "Stop" : "Start"}</button>
+    </div>
     </>
   )
 }
