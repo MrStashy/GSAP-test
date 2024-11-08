7b4fd35f670810bec1a35d5f06a61ebaf26ec488
Will Baxter <bax.will@googlemail.com>
2024-11-08 10:56:39 +0000

    Stop start

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import './App.css'
-import { gsap } from 'gsap'
-import { useGSAP } from '@gsap/react'
-import { useState } from 'react';
+import "./App.css";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useState } from "react";
 
 gsap.registerPlugin(useGSAP);
 
-
 function App() {
-  const [animate, setAnimate] = useState<boolean>(false)
+  const [animate, setAnimate] = useState<boolean>(false);
 
   useGSAP(() => {
-    gsap.to(".text", { rotation: 360, repeat: -1, duration: 5, ease: "linear" })
-  }, [animate])
+    if (animate) {
+      gsap.to(".text", {
+        rotation: "+=360",
+        repeat: -1,
+        duration: 2,
+        ease: "linear",
+      });
+    } else {
+      gsap.killTweensOf(".text"); 
+    }
+  }, [animate]);
 
   return (
     <>
-    <div className="flex flex-col h-screen items-center justify-center gap-6">
-    <p className="text">Let's go</p>
-    <button onClick={() => setAnimate(!animate)} className="border-2 p-2">{animate ? "Stop" : "Start"}</button>
-    </div>
+      <div className="flex flex-col h-screen items-center justify-center gap-6">
+        <p className="text">Let's go</p>
+        <button onClick={() => setAnimate(!animate)} className="border-2 p-2">
+          {animate ? "Stop" : "Start"}
+        </button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
